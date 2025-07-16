684074ed8cdb60f6cd8fba030b2bc182d67ada69
Sarah <80551861+s4sadev@users.noreply.github.com>
2025-07-16 17:56:50 +0000

    correcao2

--- src/App.jsx
+++ src/App.jsx
@@ -100,10 +100,10 @@ async function buscarDespesa() {
   function getTipoImg(tipo){
     switch (tipo) {
       case "Despesa":
-        return "./public/menos_card.png"
+        return "/menos_card.png"
       
       case "Receita":
-        return "./public/mais_card.png"
+        return "/mais_card.png"
 
       default:
         console.log("Tipo não identificado", tipo);
@@ -652,11 +652,11 @@ function formatarValor(valor) {
                     <span id="aside-card" className="flex flex-col max-w-[30px] justify-around max-h-[60px] min-w-[25px] min-h-[65px]">
                   
                       <button className="p-1 m-1  max-w-[25px] max-h-[25px] w-full h-full" onClick={() => removerItem(item.id)}>
-                        <img src="./public/trash.png" className="" alt="" />
+                        <img src="/trash.png" className="" alt="" />
                       </button>
 
                       <button className="p-1  m-1 max-w-[25px] max-h-[25px] w-full h-full" onClick={() => OpenModalEdit(item)} id="edit">
-                        <img src="./public/edit.png" alt="" />
+                        <img src="/edit.png" alt="" />
                       </button>
 
                     </span>                
