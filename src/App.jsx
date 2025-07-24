7353503d0e8cc96854098537c5272cedb51d5185
Sarah <80551861+s4sadev@users.noreply.github.com>
2025-07-24 20:06:04 +0000

    inicio_aplicacao_rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
-import { useEffect, useState } from 'react';
+
 import './App.css';
-import {
-  collection,
-  addDoc,
-  getDocs,
-  getDoc,
-  query,
-  where,
-  onSnapshot,
-  doc,
-  deleteDoc,
-  updateDoc,
-} from 'firebase/firestore'; //aqui podemos exportar as funções necessaria do CRUD
 import './index.css'; // ou './main.css'
-import {
-  Dialog,
-  DialogTitle,
-  DialogPanel,
-  Transition,
-  Disclosure, 
-  DisclosureButton, 
-  DisclosurePanel ,
-} from '@headlessui/react';
-
-
-import { db } from '../firebasedb';
-import Swal from 'sweetalert2';
 import Home from './Home';
+import Login from './Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <>
-      <Home></Home>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}></Route>
+      </Routes>
+    </Router>
   );
 
 
---
+++ src/Login.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+export default function Login(){ 
+    return(
+        <div class="flex justify-center flex-col w-[100%] items-center">
+            <h1 class="">Faça o seu Login</h1>
+            <form class="flex flex-col w-[50%] justify-center" action="" method="post">
+                <label htmlFor="">Usuario</label>
+                <input type="text" />
+
+                <label htmlFor="">Senha</label>
+                <input type="text" />
+
+                <span class="flex ">
+                    <button>Me cadastrar</button>
+                    <button onclick=''>Acessar</button>
+                </span>
+            </form>
+        </div>
+    )
+}
