5c9390f397ea00badc376f93357509108555a61d
Sarah <80551861+s4sadev@users.noreply.github.com>
2025-08-05 18:17:27 +0000

    .

--- service_worker.js
+++ service_worker.js
@@ -1,26 +1,13 @@
 // service-worker.js
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('meu-app-cache').then(cache => {
-      return cache.addAll([
-        '/',          // Página inicial
-        '/index.html', // SPA base
-        '/manifest.json',
-        '/favicon.ico'
-      ]);
-    })
-  );
+  console.log('Service Worker instalado com sucesso');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker ativado');
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      // Se o recurso já está no cache, retorna ele
-      if (response) return response;
-
-      // Caso contrário, tenta buscar na rede
-      return fetch(event.request).catch(() => caches.match('/index.html'));
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
