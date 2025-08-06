ba41d27f123e187c01b6a51802e6012f959f07b3
biaMP2 <beatrizalmeidapires20@gmail.com>
2025-08-06 01:48:44 +0000

    front-login-por-enquanto

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Login(){ 
     return(
         <div class="flex justify-center flex-col w-[100%] items-center">
-            <h1 class="">Faça o seu Login</h1>
+            <h1 class="title-login">Faça o seu Login</h1>
             <form class="flex flex-col w-[50%] justify-center" action="" method="post">
                 <label htmlFor="">Usuario</label>
                 <input type="text" />
@@ -12,11 +12,21 @@ export default function Login(){
                 <input type="text" />
 
                 <span class="flex ">
-
-                    <Link to="/cadastro"><button>Me cadastrar</button></Link>
-                    <Link to="/home"> <button>Acessar</button></Link>
+                <div class="btn-cadastro">
+                    <Link to="/cadastro"><button>Me cadastrar</button></Link> 
+                    
+                </div>
+                <div class="btn-acessar">
+                 <Link to="/home"> <button>Acessar</button></Link>
+                 </div>
+                   
                 </span>
+            
+
             </form>
+            
         </div>
     )
 }
+
+<link rel="stylesheet" href="style.css"></link>
